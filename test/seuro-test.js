9994dee1d5e66786a63d8a2656198b2c4e8b74a0
Ewan Sheldon <ewansheldon@gmail.com>
2022-06-22 16:04:23 +0100

    tests burner / minter role for contract owner

--- test/seuro-test.js
+++ test/seuro-test.js
@@ -2,38 +2,49 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("SEuro", function () {
-  it("Should return the new vault ID once it's deployed", async function () {
-    const [owner, address_1] = await ethers.getSigners();
+  const MR = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('MINTER_ROLE'));
+  const BR = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('BURNER_ROLE'));
+  let owner, address_1, SEuro;
 
-    const SEURO = await ethers.getContractFactory("SEuro");
-    const seuro = await SEURO.deploy("SEURO", "SEUR", [address_1.address]);
-    await seuro.deployed();
-
-    let userBalance = await seuro.balanceOf(address_1.address);
-    expect(userBalance).to.eq(0);
-
-    const value = ethers.utils.parseEther("0.5");
-
-    // mint
-    await seuro.mint(address_1.address, value);
-
-    userBalance = await seuro.balanceOf(address_1.address);
-    expect(userBalance).to.eq(value);
-
-    // burn
-    await seuro.burn(address_1.address, value);
-
-    userBalance = await seuro.balanceOf(address_1.address);
-    expect(userBalance).to.eq(0);
-  
-    const mr = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('MINTER_ROLE'));
-    expect(await seuro.hasRole(mr, owner.address)).to.eq(true);
-    expect(await seuro.hasRole(mr, address_1.address)).to.eq(true);
-    
-    const br = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('BURNER_ROLE'));
-    expect(await seuro.hasRole(br, owner.address)).to.eq(true);
-    expect(await seuro.hasRole(br, address_1.address)).to.eq(true);
+  beforeEach(async () => {
+    [owner, address_1] = await ethers.getSigners();
+    const SEuroContract = await ethers.getContractFactory("SEuro");
+    SEuro = await SEuroContract.connect(owner).deploy("sEURO", "SEUR", [address_1.address]);
+    await SEuro.deployed();
   });
+
+  describe('initialisation', async () => {
+    it('grants minter and burner role to owner', async () => {
+      expect(await SEuro.hasRole(MR, owner.address)).to.eq(true);
+      expect(await SEuro.hasRole(BR, owner.address)).to.eq(true);
+    });
+
+  });
+
+  // it("initialises token with roles", async function () {
+
+  //   let userBalance = await seuro.balanceOf(address_1.address);
+  //   expect(userBalance).to.eq(0);
+
+  //   const value = ethers.utils.parseEther("0.5");
+
+  //   // mint
+  //   await seuro.mint(address_1.address, value);
+
+  //   userBalance = await seuro.balanceOf(address_1.address);
+  //   expect(userBalance).to.eq(value);
+
+  //   // burn
+  //   await seuro.burn(address_1.address, value);
+
+  //   userBalance = await seuro.balanceOf(address_1.address);
+  //   expect(userBalance).to.eq(0);
+  
+  //   expect(await seuro.hasRole(mr, address_1.address)).to.eq(true);
+    
+  //   expect(await seuro.hasRole(br, owner.address)).to.eq(true);
+  //   expect(await seuro.hasRole(br, address_1.address)).to.eq(true);
+  // });
 
 
   // TODO test ownership and roles
