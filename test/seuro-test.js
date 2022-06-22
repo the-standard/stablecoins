3f4417d415d06320a9d9b327cf3eee111fa28677
Ewan Sheldon <ewansheldon@gmail.com>
2022-06-22 16:24:41 +0100

    test revoking roles

--- test/seuro-test.js
+++ test/seuro-test.js
@@ -4,12 +4,12 @@ const { ethers } = require('hardhat');
 describe('SEuro', function () {
   const MR = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('MINTER_ROLE'));
   const BR = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('BURNER_ROLE'));
-  let owner, address_1, address_2, SEuro;
+  let owner, admin, non_admin, SEuro;
 
   beforeEach(async () => {
-    [owner, address_1, address_2] = await ethers.getSigners();
+    [owner, admin, non_admin] = await ethers.getSigners();
     const SEuroContract = await ethers.getContractFactory('SEuro');
-    SEuro = await SEuroContract.connect(owner).deploy('sEURO', 'SEUR', [address_1.address]);
+    SEuro = await SEuroContract.connect(owner).deploy('sEURO', 'SEUR', [admin.address]);
     await SEuro.deployed();
   });
 
@@ -25,63 +25,78 @@ describe('SEuro', function () {
     });
 
     it('does not grand minter / burner role for non-admins', async () => {
-      expect(await SEuro.hasRole(MR, address_2.address)).to.eq(false);
-      expect(await SEuro.hasRole(BR, address_2.address)).to.eq(false);
+      expect(await SEuro.hasRole(MR, non_admin.address)).to.eq(false);
+      expect(await SEuro.hasRole(BR, non_admin.address)).to.eq(false);
     });
   });
 
   describe('minting', async () => {
     it('reverts when minted by non-admin', async () => {
       const value = ethers.utils.parseEther('0.5');
-      const mint = SEuro.connect(address_2).mint(address_1.address, value);
+      const mint = SEuro.connect(non_admin).mint(admin.address, value);
 
       await expect(mint).to.be.revertedWith('invalid-minter');
-      expect(await SEuro.balanceOf(address_1.address)).to.eq(0);
+      expect(await SEuro.balanceOf(admin.address)).to.eq(0);
     });
 
     it('mints when performed by admin', async () => {
       const value = ethers.utils.parseEther('0.5');
-      const mint = SEuro.connect(address_1).mint(address_1.address, value);
+      const mint = SEuro.connect(admin).mint(admin.address, value);
 
       await expect(mint).not.to.be.revertedWith('invalid-minter');
-      expect(await SEuro.balanceOf(address_1.address)).to.eq(value);
+      expect(await SEuro.balanceOf(admin.address)).to.eq(value);
     });
   });
 
   describe('burning', async () => {
     it('reverts when burned by non-admin', async () => {
       const value = ethers.utils.parseEther('0.5');
-      await SEuro.connect(address_1).mint(address_1.address, value);
+      await SEuro.connect(admin).mint(admin.address, value);
 
-      const burn = SEuro.connect(address_2).burn(address_1.address, value);
+      const burn = SEuro.connect(non_admin).burn(admin.address, value);
 
       await expect(burn).to.be.revertedWith('invalid-burner');
-      expect(await SEuro.balanceOf(address_1.address)).to.eq(value);
+      expect(await SEuro.balanceOf(admin.address)).to.eq(value);
     });
 
     it('burns when performed by admin', async () => {
       const value = ethers.utils.parseEther('0.5');
-      await SEuro.connect(address_1).mint(address_1.address, value);
+      await SEuro.connect(admin).mint(admin.address, value);
 
-      const burn = SEuro.connect(address_1).burn(address_1.address, value);
+      const burn = SEuro.connect(admin).burn(admin.address, value);
 
       await expect(burn).not.to.be.revertedWith('invalid-burner');
-      expect(await SEuro.balanceOf(address_1.address)).to.eq(0);
+      expect(await SEuro.balanceOf(admin.address)).to.eq(0);
     });
   });
 
   describe('granting roles', async () => {
-    it('allows contract owner to add roles', async () => {
-      await SEuro.connect(owner).grantRole(MR, address_2.address);
+    it('allows contract owner to grant roles', async () => {
+      await SEuro.connect(owner).grantRole(MR, non_admin.address);
 
-      expect(await SEuro.hasRole(MR, address_2.address)).to.eq(true);
+      expect(await SEuro.hasRole(MR, non_admin.address)).to.eq(true);
     });
 
-    it('does not allow non-owner to add roles', async () => {
-      const grant = SEuro.connect(address_1).grantRole(MR, address_2.address);
+    it('does not allow non-owner to grant roles', async () => {
+      const grant = SEuro.connect(admin).grantRole(MR, non_admin.address);
 
       await expect(grant).to.be.reverted;
-      expect(await SEuro.hasRole(MR, address_2.address)).to.eq(false);
+      expect(await SEuro.hasRole(MR, non_admin.address)).to.eq(false);
+    });
+  });
+
+  describe('revoking roles', async () => {
+    it('allows contract owner to revoke roles', async () => {
+      await SEuro.connect(owner).revokeRole(MR, admin.address);
+
+      expect(await SEuro.hasRole(MR, admin.address)).to.eq(false);
+    });
+
+    it('does not allow non-owner to revoke roles', async () => {
+      const revoke = SEuro.connect(non_admin).revokeRole(MR, admin.address);
+
+      await expect(revoke).to.be.reverted;
+      expect(await SEuro.hasRole(MR, admin.address)).to.eq(true);
     });
   });
 });
