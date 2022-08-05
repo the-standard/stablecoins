73a24f03e6ec17f2d56d2c0db13db2dfc599587c
dkf <dannzor@outlook.com>
2022-08-05 10:25:27 +0100

    use protected role functions

--- contracts/SEURO.sol
+++ contracts/SEURO.sol
@@ -7,14 +7,13 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract SEuro is ERC20, AccessControl {
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
   bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
-  bytes32 public constant DAO_ADMIN = keccak256("DAO_ADMIN");
 
   constructor(string memory name, string memory symbol, address[] memory _admins)
       ERC20(name, symbol)
   {
     _grantRole(MINTER_ROLE, msg.sender);
     _grantRole(BURNER_ROLE, msg.sender);
-    _grantRole(DAO_ADMIN, msg.sender);
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
 
     for(uint8 i=0; i<_admins.length; i++){
       _grantRole(MINTER_ROLE, _admins[i]);
@@ -22,22 +21,17 @@ contract SEuro is ERC20, AccessControl {
     }
   }
 
-  modifier onlyAdmin() {
-      require(hasRole(DAO_ADMIN, msg.sender), "invalid-admin");
-      _;
-  }
-
   function mint(address to, uint256 amount) public {
     require(hasRole(MINTER_ROLE, msg.sender), "invalid-minter");
     _mint(to, amount);
   }
 
-  function addMinter(address _address) public onlyAdmin {
-      _grantRole(MINTER_ROLE, _address);
+  function addMinter(address _address) public {
+      grantRole(MINTER_ROLE, _address)
   }
 
-  function removeMinter(address _address) public onlyAdmin {
-      _revokeRole(MINTER_ROLE, _address);
+  function removeMinter(address _address) public {
+      revokeRole(MINTER_ROLE, _address);
   }
 
   function burn(address from, uint256 amount) public {
@@ -45,12 +39,12 @@ contract SEuro is ERC20, AccessControl {
     _burn(from, amount);
   }
 
-  function addBurner(address _address) public onlyAdmin {
-      _grantRole(BURNER_ROLE, _address);
+  function addBurner(address _address) public {
+      grantRole(BURNER_ROLE, _address);
   }
 
-  function removeBurner(address _address) public onlyAdmin {
-      _revokeRole(BURNER_ROLE, _address);
+  function removeBurner(address _address) public {
+      revokeRole(BURNER_ROLE, _address);
   }
 
 }
