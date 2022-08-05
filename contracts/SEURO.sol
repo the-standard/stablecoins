02f4ab97c6dc62ad9f3e1115cc7258533a6a846b
dkf <35889839+therealdannzor@users.noreply.github.com>
2022-08-05 10:41:18 +0100

    Add & remove external minters and burners (#2)
    * extend the granting of roles
    
    * change name for default role
    
    * use protected role functions
    
    * minor syntax fix

--- contracts/SEURO.sol
+++ contracts/SEURO.sol
@@ -4,20 +4,15 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-// import "hardhat/console.sol";
-
 contract SEuro is ERC20, AccessControl {
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
   bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
-  // bytes32 public constant DEFAULT_ADMIN_ROLE = keccak256("DEFAULT_ADMIN_ROLE");
 
   constructor(string memory name, string memory symbol, address[] memory _admins)
-      public
       ERC20(name, symbol)
   {
     _grantRole(MINTER_ROLE, msg.sender);
     _grantRole(BURNER_ROLE, msg.sender);
-
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
 
     for(uint8 i=0; i<_admins.length; i++){
@@ -31,17 +26,25 @@ contract SEuro is ERC20, AccessControl {
     _mint(to, amount);
   }
 
+  function addMinter(address _address) public {
+      grantRole(MINTER_ROLE, _address);
+  }
+
+  function removeMinter(address _address) public {
+      revokeRole(MINTER_ROLE, _address);
+  }
+
   function burn(address from, uint256 amount) public {
     require(hasRole(BURNER_ROLE, msg.sender), "invalid-burner");
     _burn(from, amount);
   }
 
-  // function grantRoleMint(address admin) {
-  // }
+  function addBurner(address _address) public {
+      grantRole(BURNER_ROLE, _address);
+  }
 
-  // TODO who are minters - this should be a contract that's controlled by tsd
-  // TODO add minters?
-  // TODO add burners?
-  // TODO remove them!
-  // view owners
+  function removeBurner(address _address) public {
+      revokeRole(BURNER_ROLE, _address);
+  }
+
 }
